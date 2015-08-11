9e08a9d0cfa1feef9fbea4a21162366d50fa583c
Fabian Buitrago <fabian.buitrago@yuxipacific.com>
2015-08-11 16:59:19 -0500

    Tokens

--- js/ajaxSetup.js
+++ js/ajaxSetup.js
@@ -1,12 +1,17 @@
 $.ajaxSetup({
-    headers: {'X-CSRF-Token':  sessionStorage['token'] ? sessionStorage['token'] : ''},
     beforeSend:function(xhr, settings){
+      var token = sessionStorage['token'] ? sessionStorage['token'] : '';
+
+      if(settings.url.indexOf("login") < 0){
+        xhr.setRequestHeader("Authorization", "x-csrf-token="+token);
+      }
+
       $.blockUI();
     },
     complete: function (){
       $.unblockUI();
     },
-    error: function(){
-      alert('Connection failure');
+    error: function(e){
+      alert('Error: '+e.status+" "+e.statusText);
     }
 });
